31088a2e7a4029cb9de5cf49463c1a9527753edb
wnt0801 <1299209404@qq.com>
2026-04-29 15:17:18 +0800

    add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,30 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://www.wannantian.com',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+    {
+      url: 'https://www.wannantian.com/projects/olist',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://www.wannantian.com/projects/olist-cancellation',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://www.wannantian.com/projects/a-stock',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+  ]
+}
